91d383fccb9e813f6ada27cbb8c9aabe514752b4
ciyuxu <ciyuxu@gmail.com>
2017-10-31 21:20:53 +0100

    Change UpdateEvaluation Hook

--- src/hooks/updateEvaluation.js
+++ src/hooks/updateEvaluation.js
@@ -1,22 +1,63 @@
-const moment = require('moment');
+const ADD_EVALUATION = 'ADD_EVALUATION'
+const EDIT_EVALUATION = 'EDIT_EVALUATION'
 
-module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
-  return function (hook) {
-    return hook.app.service('students').get(hook.id)
-      .then ((student) => {
-        Object.assign(
-          {},
-          student,
-          student.evaluations = [
-            {
-              evaluationDate: moment().format('DD MM YYYY'),
-              currentColor: hook.data.color,
-              remark: hook.data.remark,
+
+module.exports = function (options = {}) {
+  return function evaluateStudent (hook) {
+    // Hooks can either return nothing or a promise
+    // that resolves with the `hook` object for asynchronous operations
+    return  hook.app.service('students').get(hook.id)
+      .then((student) => {
+
+        const { type, payload } = hook.data;
+        console.log('"payload:"', type)
+
+
+        switch (type) {
+        case ADD_EVALUATION:{
+
+          if (payload.green) {
+            console.log("GREEN")
+
+            hook.data = {
+              evaluationDays: student.days.concat({day: payload.date, color: 'green', comment: payload.comment}),
+              currentColor: 'green'
             }
-          ].concat(student.evaluations)
-        );
-        hook.data = student;
-        return Promise.resolve(hook);
+            return hook;
+          }
+          if (payload.yellow) {
+            console.log("YELLOW")
+
+            hook.data = {
+              evaluationDays: student.days.concat({day: payload.date, color: 'green', comment: payload.comment}),
+              currentColor: 'yellow'
+            }
+            return hook;
+          }
+
+          if (payload.red) {
+            console.log("RED")
+
+            hook.data = {
+              evaluationDays: student.days.concat({day: payload.date, color: 'green', comment: payload.comment}),
+              currentColor: 'red'
+            }
+            return hook;
+          }
+
+          // return hook;
+        }
+
+
+        default:
+          console.log("hook",hook.id)
+          return Promise.resolve(hook);
+
+        }
+
+
       });
+
+
   };
 };
