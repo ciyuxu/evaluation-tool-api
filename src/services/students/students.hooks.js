e357ad41c5489256dbcf55f8dc2f568eea822b25
ciyuxu <ciyuxu@gmail.com>
2017-11-15 17:08:22 +0100

    No need for UpdateEvaluation

--- src/services/students/students.hooks.js
+++ src/services/students/students.hooks.js
@@ -1,44 +1,42 @@
-const { authenticate } = require('feathers-authentication').hooks;
-const commonHooks = require('feathers-hooks-common');
-
-const updateEvaluation = require('../../hooks/updateEvaluation');
+const { authenticate } = require("feathers-authentication").hooks;
+const commonHooks = require("feathers-hooks-common");
 
 const classroomSchema = {
-    include: {
-        service: 'classrooms',
-        nameAs: 'classroom',
-        parentField: 'classroomId',
-        childField: '_id'
-    }
+  include: {
+    service: "classrooms",
+    nameAs: "classroom",
+    parentField: "classroomId",
+    childField: "_id"
+  }
 };
 module.exports = {
-    before: {
-        all: [authenticate('jwt')],
-        find: [],
-        get: [],
-        create: [],
-        update: [updateEvaluation()],
-        patch: [updateEvaluation()],
-        remove: []
-    },
+  before: {
+    all: [authenticate("jwt")],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
 
-    after: {
-        all: [commonHooks.populate({ schema: classroomSchema })],
-        find: [],
-        get: [],
-        create: [],
-        update: [],
-        patch: [],
-        remove: []
-    },
+  after: {
+    all: [commonHooks.populate({ schema: classroomSchema })],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
 
-    error: {
-        all: [],
-        find: [],
-        get: [],
-        create: [],
-        update: [],
-        patch: [],
-        remove: []
-    }
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  }
 };
